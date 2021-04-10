66d448889a3e22e7142340a4e9b35cafaa8dbbdb
Esmaeil Ahmadipour <software8686@gmail.com>
2021-04-10 12:36:31 +0430

    add: Model > News-Article

---
+++ lib/models/news_article.dart
@@ -0,0 +1,30 @@
+class NewsArticle {
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  NewsArticle(
+      {this.author,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
+
+  factory NewsArticle.fromJson(Map<String, dynamic> json) {
+    return NewsArticle(
+      author: json['author'],
+      title: json['author'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+}
