8a2570840e5ce8799c9400df1ec0bc5331b21e71
Esmaeil Ahmadipour <software8686@gmail.com>
2021-04-10 12:41:46 +0430

    add: ViewModel > News-Article-View-Model

---
+++ lib/viewModel/news_article_view_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_mvvm/models/news_article.dart';
+
+class NewsArticleViewModel {
+  NewsArticle _newsArticle;
+
+  NewsArticleViewModel({NewsArticle article}) : _newsArticle = article;
+
+  String get d {
+    return _newsArticle.url;
+  }
+
+  String get url {
+    return _newsArticle.url;
+  }
+
+  String get title {
+    return _newsArticle.title;
+  }
+
+  String get author {
+    return _newsArticle.author;
+  }
+
+  String get content {
+    return _newsArticle.content;
+  }
+
+  String get description {
+    return _newsArticle.description;
+  }
+
+  String get publishedAt {
+    return _newsArticle.publishedAt;
+  }
+
+  String get urlToImage {
+    return _newsArticle.urlToImage;
+  }
+}
